c5f847906c0bc4ccd79fa4aeed927cd568617e65
wangzx-github <mrwangzx@aliyun.com>
2021-06-28 16:26:21 +0800

    hot-fix commit 1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world");
         System.out.println("hello world");
+        System.out.println("hello world");
     }
 }
