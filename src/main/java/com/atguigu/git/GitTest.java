f66fd2357563fbd4066b4f999c8c55f89908eeba
wangzx-github <76716796+wangzx-github@users.noreply.github.com>
2021-06-28 17:07:45 +0800

    Update GitTest.java

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -13,5 +13,6 @@ public class GitTest {
         System.out.println("hello world");
         System.out.println("hello world master");
         System.out.println("push test hot-fix");
+        System.out.println("pull test");
     }
 }
