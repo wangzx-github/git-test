d265f1bf4ada3590dcfc302c9c14e9b64e29bfad
wangzx-github <mrwangzx@aliyun.com>
2021-06-28 16:07:33 +0800

    first commit

---
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -0,0 +1,12 @@
+package com.atguigu.git;
+
+/**
+ * @author wangzx
+ * @create 2021-06-28-3:59 下午
+ */
+public class GitTest {
+
+    public static void main(String[] args){
+        System.out.println("hello world");
+    }
+}
